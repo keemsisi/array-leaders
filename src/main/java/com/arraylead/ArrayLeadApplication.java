c8fd9902f8b33c4b9a5e3fb7b96aaea86609cf96
keemsisi <keemsisi@gmail.com>
2019-11-18 16:11:23 +0100

    initial commit

---
+++ src/main/java/com/arraylead/ArrayLeadApplication.java
@@ -0,0 +1,15 @@
+package com.arraylead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ArrayLeadApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
